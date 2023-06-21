536b53c3e2209bc17f0938a05fe229aa19a44685
Ubuntu <ubuntu@ip-172-31-43-20.us-west-2.compute.internal>
2023-06-21 19:05:16 +0000

    finish the config

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const http = require('http');
 const path = require('path');
 const busboy = require('busboy');
 const fs = require('fs');
+const uid = require('uid');
+const jwt = require('jsonwebtoken');
+const prompt = require('prompt-sync')();
 
 //==============================
 //guaca node
@@ -9,6 +12,7 @@ const fs = require('fs');
 //  (1) ip, port
 //  (2) token, id
 //  (3) compacity: RAM, SSD, CPU
+//  (4) also, this registration needs to be a thing that regularly verify the health of the node as well.
 //2) file upload server
 //  (1) zip files can be uploaded
 //  (2) meta info of the file
@@ -35,6 +39,79 @@ const fs = require('fs');
 //  (7) stopping
 //  (8) stopped
 //  (9) stopped and cleaned
+
+// 1) register a node to the guaca client
+// 1.1) read config file
+function read_config(update_flag) {
+  fs.readFile('./config.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // 1.2) update config file if needed
+
+    console.log(data);
+    config_json = JSON.parse(data);
+    let change_flag = false;
+    if (!config_json['node-info'].uid && update_flag) {
+      config_json['node-info'].uid = uid.uid();
+      console.log('config_json.node-info.uid', config_json['node-info'].uid);
+      change_flag = true;
+    }
+    if (!config_json['node-info']['access-token'] && update_flag) {
+      let token = jwt.sign({ _id: config_json.uid }, 'SuperSecret');
+      config_json['node-info']['access-token'] = token;
+      console.log('config_json.node-info.access-token', config_json['node-info']['access-token']);
+      change_flag = true;
+    }
+
+    if (!config_json['node-info']['ip'] && update_flag) {
+      const ip = prompt('What is the external IP address?');
+      config_json['node-info']['ip'] = ip;
+      console.log('config_json.node-info.ip', config_json['node-info']['ip']);
+      change_flag = true;
+    }
+    if (!config_json['node-info']['api-port'] && update_flag) {
+      const port = prompt('What is the external Port for api connection?');
+      config_json['node-info']['api-port'] = port;
+      console.log('config_json.node-info.api-port', config_json['node-info']['api-port']);
+      change_flag = true;
+    }
+    if (!config_json['node-info']['RAM(MB)'] && update_flag) {
+      const ram = prompt('What is the amount of RAM allowed to be used by the node (in MB)?');
+      config_json['node-info']['RAM(MB)'] = ram;
+      console.log('config_json.node-info.RAM(MB)', config_json['node-info']['RAM(MB)']);
+      change_flag = true;
+    }
+    if (!config_json['node-info']['SSD(MB)'] && update_flag) {
+      const ssd = prompt('What is the amount of SSD space allowed to be used by the node (in MB)?');
+      config_json['node-info']['SSD(MB)'] = ssd;
+      console.log('config_json.node-info.SSD(MB)', config_json['node-info']['SSD(MB)']);
+      change_flag = true;
+    }
+    if (!config_json['node-info']['CPU Cores'] && update_flag) {
+      const cpus = prompt('What is the number of CPU Cores allowed to be used by the node?');
+      config_json['node-info']['CPU Cores'] = cpus;
+      console.log('config_json.node-info.CPU Cores', config_json['node-info']['CPU Cores']);
+      change_flag = true;
+    }
+
+    if (change_flag) {
+      let config_txt = JSON.stringify(config_json, null, 4);
+      fs.writeFile('./config.json', config_txt, (err) => {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+      });
+    }
+
+  });
+}
+
+let update_flag = true;
+read_config(update_flag);
+
+
 http.createServer((req, res) => {
   if (req.method === 'POST') {
     console.log('POST request');
