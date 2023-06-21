2e1a25f020a4bf7fe6b32e2979bf1465146e5f41
Titan Turtles <titanturtlesteam@gmail.com>
2023-06-21 16:56:21 -0700

    add health check information

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const uid = require('uid');
 const jwt = require('jsonwebtoken');
 const prompt = require('prompt-sync')();
+const cron = require('node-cron');
+const axios = require('axios');
+const { config } = require('process');
 
 //==============================
 //guaca node
@@ -42,6 +45,7 @@ const prompt = require('prompt-sync')();
 
 // 1) register a node to the guaca client
 // 1.1) read config file
+let config_json = null;
 function read_config(update_flag) {
   fs.readFile('./config.json', 'utf8', (err, data) => {
     if (err) {
@@ -54,46 +58,71 @@ function read_config(update_flag) {
     console.log(data);
     config_json = JSON.parse(data);
     let change_flag = false;
-    if (!config_json['node-info'].uid && update_flag) {
-      config_json['node-info'].uid = uid.uid();
-      console.log('config_json.node-info.uid', config_json['node-info'].uid);
+    if (!config_json.node_info.uid && update_flag) {
+      config_json.node_info.uid = uid.uid();
+      console.log('config_json.node_info.uid', config_json.node_info.uid);
       change_flag = true;
     }
-    if (!config_json['node-info']['access-token'] && update_flag) {
+    if (!config_json.node_info.access_token && update_flag) {
       let token = jwt.sign({ _id: config_json.uid }, 'SuperSecret');
-      config_json['node-info']['access-token'] = token;
-      console.log('config_json.node-info.access-token', config_json['node-info']['access-token']);
+      config_json.node_info.access_token = token;
+      console.log('config_json.node_info.access_token', config_json.node_info.access_token);
       change_flag = true;
     }
 
-    if (!config_json['node-info']['ip'] && update_flag) {
+    if (!config_json.node_info.ip && update_flag) {
       const ip = prompt('What is the external IP address?');
-      config_json['node-info']['ip'] = ip;
-      console.log('config_json.node-info.ip', config_json['node-info']['ip']);
+      config_json.node_info.ip = ip;
+      console.log('config_json.node_info.ip', config_json.node_info.ip);
       change_flag = true;
     }
-    if (!config_json['node-info']['api-port'] && update_flag) {
+    if (!config_json.node_info.api_port && update_flag) {
       const port = prompt('What is the external Port for api connection?');
-      config_json['node-info']['api-port'] = port;
-      console.log('config_json.node-info.api-port', config_json['node-info']['api-port']);
+      config_json.node_info.api_port = parseInt(port);
+      console.log('config_json.node_info.api_port', config_json.node_info.api_port);
       change_flag = true;
     }
-    if (!config_json['node-info']['RAM(MB)'] && update_flag) {
+    if (!config_json.node_info.ram_mb && update_flag) {
       const ram = prompt('What is the amount of RAM allowed to be used by the node (in MB)?');
-      config_json['node-info']['RAM(MB)'] = ram;
-      console.log('config_json.node-info.RAM(MB)', config_json['node-info']['RAM(MB)']);
+      config_json.node_info.ram_mb = parseInt(ram);
+      console.log('config_json.node_info.ram_mb', config_json.node_info.ram_mb);
       change_flag = true;
     }
-    if (!config_json['node-info']['SSD(MB)'] && update_flag) {
+    if (!config_json.node_info.ssd_mb && update_flag) {
       const ssd = prompt('What is the amount of SSD space allowed to be used by the node (in MB)?');
-      config_json['node-info']['SSD(MB)'] = ssd;
-      console.log('config_json.node-info.SSD(MB)', config_json['node-info']['SSD(MB)']);
+      config_json.node_info.ssd_mb = parseInt(ssd);
+      console.log('config_json.node_info.ssd_mb', config_json.node_info.ssd_mb);
       change_flag = true;
     }
-    if (!config_json['node-info']['CPU Cores'] && update_flag) {
-      const cpus = prompt('What is the number of CPU Cores allowed to be used by the node?');
-      config_json['node-info']['CPU Cores'] = cpus;
-      console.log('config_json.node-info.CPU Cores', config_json['node-info']['CPU Cores']);
+    if (!config_json.node_info.cpu_cores && update_flag) {
+      const cpus = prompt('What is the number of cpu_cores allowed to be used by the node?');
+      config_json.node_info.cpu_cores = parseInt(cpus);
+      console.log('config_json.node_info.cpu_cores', config_json.node_info.cpu_cores);
+      change_flag = true;
+    }
+    if (!config_json.node_info.image_port_low && update_flag) {
+      const image_port_low = prompt('What is the lower bound of port of the image for the node?');
+      config_json.node_info.image_port_low = image_port_low;
+      console.log('config_json.node_info.image_port_low', config_json.node_info.image_port_low);
+      change_flag = true;
+    }
+    if (!config_json.node_info.image_port_up && update_flag) {
+      const image_port_up = prompt('What is the upper bound of port of the image for the node?');
+      config_json.node_info.image_port_up = image_port_up;
+      console.log('config_json.node_info.image_port_up', config_json.node_info.image_port_up);
+      change_flag = true;
+    }
+
+    if (!config_json.node_info.guace_hive_ip && update_flag) {
+      const guace_hive_ip = prompt('What is the (ip) address for guaca hive?');
+      config_json.node_info.guace_hive_ip = guace_hive_ip;
+      console.log('config_json.node_info.guace_hive_ip', config_json.node_info.guace_hive_ip);
+      change_flag = true;
+    }
+    if (!config_json.node_info.guaca_hive_port && update_flag) {
+      const guaca_hive_port = prompt('What is the port for guaca hive?');
+      config_json.node_info.guaca_hive_port = guaca_hive_port;
+      console.log('config_json.node_info.guaca_hive_port', config_json.node_info.guaca_hive_port);
       change_flag = true;
     }
 
@@ -111,6 +140,27 @@ function read_config(update_flag) {
 let update_flag = true;
 read_config(update_flag);
 
+function health_update() {
+  console.log('called health_update');
+  const data = config_json;
+
+  axios.post('http://lab.titanturtles.org:20002/api/health', data)
+    .then((res) => {
+        console.log(`Status: ${res.status}`);
+        console.log('Body: ', res.data);
+    }).catch((err) => {
+        console.error(err);
+    });
+}
+
+let seconds = '' + (Math.random() * 60);
+cron.schedule(seconds + ' * * * * *', () => {
+  health_update();
+});
+
+setTimeout(()=> {
+  health_update();
+}, 300);
 
 http.createServer((req, res) => {
   if (req.method === 'POST') {
